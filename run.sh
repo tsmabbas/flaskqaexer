d9487c338ca2cf365e81e5daa5e0cd734681a0db
tsmabbas <99358233+tsmabbas@users.noreply.github.com>
2022-06-28 15:04:48 +0100

    MA : All install in separate line

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
-sudo apt install python3 python3-venv python3-pip
+sudo apt install python3
+sudo apt install python3-venv 
+sudo apt install python3-pip
 sudo python3 -m venv venv
 sudo source venv/bin/activate
 sudo pip install -r requirements.txt
